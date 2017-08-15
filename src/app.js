5659d11d3d043399c64d34532d7a0a8f9ea494d0
Shaun Scovil <sscovil@circle.com>
2017-08-15 14:39:17 -0400

    Fix app.js to serve static files

--- src/app.js
+++ src/app.js
@@ -2,12 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.get('/', function (req, res) {
-     res.send('Hello World!')
-})
-
-app.listen(3000, function() {
-     console.log('Example app listening on port 3000!')
-})
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(PORT, function() {
+  console.log(`Server is listening on port ${PORT}!`);
+});
