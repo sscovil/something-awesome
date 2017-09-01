35676461c8cb366995e0f07aec248d4cf1f0d415
George Famutimi <gof3@cornell.edu>
2017-08-31 18:42:13 -0700

    added code in order to try and add module in src/mailer.js

--- src/mailer.js
+++ src/mailer.js
@@ -4,12 +4,16 @@ const domain = 'sandbox6eddd0f1d9eb4fddbcccea158cc97fd6.mailgun.org';
 const mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
 
 var data = {
-  from: 'Excited User <me@samples.mailgun.org>',
-  to: 'serobnic@mail.ru',
-  subject: 'Hello',
-  text: 'Testing some Mailgun awesomeness!'
+  from: body.email,
+  to: 'me@'+domain,
+  subject: body.subject,
+  text: body.message
 };
 
-mailgun.messages().send(data, function (error, body) {
-  console.log(body);
-});
+
+
+exports.sendText = function(to, from, subject, message, callback) {
+  mailgun.messages().send(data, function (error, body) {
+    console.log(body);
+  });
+};
