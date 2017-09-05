6befe8bce6db3ea1066fcba9de5f93d1cbb37f10
George Famutimi <gof3@cornell.edu>
2017-09-05 13:08:02 -0700

    saving work from last week so that I can test Shaun's solution and compare

--- src/app.js
+++ src/app.js
@@ -1,14 +1,44 @@
 require('dotenv').config();
+const mailer = require('./mailer');
 
 const express = require('express');
 const path = require('path');
 const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.PORT;
+const mailgun_recipient = process.env.MAILGUN_RECIPIENT;
 
 app.post('/api/forms/contact', bodyParser.urlencoded({extended: true}), function(req, res) {
-  console.log(req.body);
-  res.sendStatus(204);
+  let data = {
+    to: mailgun_recipient,
+    from: req.body.email,
+    subject: req.body.subject,
+    message: req.body.message,
+  }
+
+//  mailer.sendText(to, from, subject, message, function (err, body)){
+//    if(err) {
+//      return res.status(400).send(err.messge);
+//    }
+//    else {
+//      return res.sendStatus(204);
+//      console.log(req.body);
+//    }
+//  }
+
+  }
+);
+
+  mailer.sendText(data, function(err,body) {
+
+
+    mailgun.messages().send(data, function (error, body) {
+    if(err) {
+      return res.status(400).send(err.message);
+      }
+    else {
+    console.log(res.body);}
+    });
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
--- src/mailer.js
+++ src/mailer.js
@@ -3,16 +3,15 @@ const mailgun_recipient = process.env.MAILGUN_RECIPIENT;
 const domain = 'sandbox6eddd0f1d9eb4fddbcccea158cc97fd6.mailgun.org';
 const mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
 
-var data = {
-  from: body.email,
-  to: 'me@'+domain,
-  subject: body.subject,
-  text: body.message
-};
-
-
-
 exports.sendText = function(to, from, subject, message, callback) {
+  var data = {
+    from: body.email,
+    to: mailgun_recipient,
+    subject: body.subject,
+    text: body.message
+  };
+
+
   mailgun.messages().send(data, function (error, body) {
     console.log(body);
   });
