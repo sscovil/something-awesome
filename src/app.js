6cc77cdbb3f31f3b6df2aeca51e7431867ad4ce3
George Famutimi <gof3@cornell.edu>
2017-09-11 13:20:04 -0700

    adding 'use strict' to top of both modules

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 require('dotenv').config();
 const mailer = require('./mailer');
 
--- src/mailer.js
+++ src/mailer.js
@@ -1,10 +1,12 @@
+'use strict';
+
 const api_key = process.env.MAILGUN_SECRET;
 const mailgun_recipient = process.env.MAILGUN_RECIPIENT;
 const domain = 'sandbox6eddd0f1d9eb4fddbcccea158cc97fd6.mailgun.org';
 const mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
 
 exports.sendText = function(to, from, subject, message, callback) {
-  var data = {
+  const data = {
     from: body.email,
     to: mailgun_recipient,
     subject: body.subject,
