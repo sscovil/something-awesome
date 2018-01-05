cf7135b9c518a25a6397f68e6ee1cebba6dfe35d
George Famutimi <gof3@cornell.edu>
2018-01-05 10:58:45 -0500

    Deleted merge conflict

--- migrations/002-create-table-posts.js
+++ migrations/002-create-table-posts.js
@@ -1,22 +1,10 @@
 'use strict';
 
 exports.up = (knex) => {
-<<<<<<< HEAD
-<<<<<<< HEAD
-  return knex.schema.createTableIfNotExists('posts', function (table) {
-    table.increments('id', 1);
-    table.text('title', 'My First Post');
-    table.text('content', `<p>
-      Bud Bud Bud Bud Bud Bud Bud Bud Bud Bud Bud Bud Bud Bud Bud Bud Bud</p>`
-    );
-=======
-=======
->>>>>>> cb06cebaf5f020b9ca9d1b4fab42e3618864fca5
   return knex.schema.createTableIfNotExists('posts', (table) => {
     table.increments('id');
     table.text('title');
     table.text('content');
->>>>>>> cb06cebaf5f020b9ca9d1b4fab42e3618864fca5
   });
 };
 
